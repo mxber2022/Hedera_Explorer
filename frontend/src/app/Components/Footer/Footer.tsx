f905bab5a46402099e9ca6162ef423f4ceb4d357
mxber2022 <mxber2022@gmail.com>
2024-08-18 04:11:00 +0200

    Update Footer.tsx

--- frontend/src/app/Components/Footer/Footer.tsx
+++ frontend/src/app/Components/Footer/Footer.tsx
@@ -8,6 +8,16 @@ function Footer() {
                 <div className="footer__copyright">
                    © Copyright 2024
                 </div>
+                <div className="footer__links">
+                    <a href="https://hedera.com/" target="_blank" rel="noopener noreferrer">Website</a>
+                    <a href="https://x.com/hedera" target="_blank" rel="noopener noreferrer">X</a>
+                    <a href="https://www.facebook.com/hederanetwork/" target="_blank" rel="noopener noreferrer">Facebook</a>
+                    <a href="https://www.facebook.com/hederanetwork/" target="_blank" rel="noopener noreferrer">Telegram</a>
+                    <a href="https://discord.com/invite/uJ5k8DkmKV" target="_blank" rel="noopener noreferrer">Discord</a>
+                    <a href="https://www.youtube.com/hederahashgraph" target="_blank" rel="noopener noreferrer">YouTube</a>
+                    <a href="https://github.com/hashgraph" target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a href="https://medium.com/hedera" target="_blank" rel="noopener noreferrer">Medium</a>
+                </div>
             </div>
         </footer>
     )
